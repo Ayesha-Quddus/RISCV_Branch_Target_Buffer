be970d8696f7750df5e637a79816ad24c8c57a29
Ayesha Quddus <ayeshaquddus111@gmail.com>
2025-10-11 14:20:45 +0100

    Added update handling in BTB module

--- src/btb.sv
+++ src/btb.sv
@@ -11,14 +11,20 @@ module btb(
 );
 
     // ----------- IF Stage -------------
+
     // PC (32 bits) = Tag (27 bits) + Index (3 bits) + Byte offset (2 bits)
     logic [2:0]  index = PC[4:2];
     logic [26:0] tag   = PC[31:5];
 
     // BTB memory
     logic [127:0] read_set;
+
+    logic [127:0] update_set;
+    logic [2:0]   update_index = updatePC[4:2];
+    logic [26:0]  update_tag   = updatePC[31:5];
+
     logic [127:0] write_set;
-    logic [2:0]   write_index;
+    logic [2:0]   write_index = update_index;
 
     btb_file u_btb_file(
         .clk(clk),
@@ -26,7 +32,9 @@ module btb(
         .read_set(read_set),
         .write_index(write_index),
         .write_set(write_set),
-        .write_enable(update)
+        .write_enable(update),
+        .update_index(update_index),   
+        .update_set(update_set)
     );
 
     // Read branch entries and compare tags using btb_read_logic
@@ -42,5 +50,23 @@ module btb(
 
     assign predictedTaken = current_fsm[1];
 
-    
+
+    // ----------- EX Stage ------------
+
+    // Use btb_read_logic to read the set for update
+    logic [31:0] update_target;
+    logic [1:0]  update_fsm;
+    logic        update_hit;
+
+    btb_read_logic u_update_read(
+        .set_data(update_set),
+        .pc_tag(update_tag),   
+        .hit(update_hit),      
+        .target(update_target),
+        .fsm_state(update_fsm) 
+    );
+
+    logic new_entry = ~update_hit; // if no hit, this is a new entry
+
+   
 endmodule
--- src/btb_file.sv
+++ src/btb_file.sv
@@ -1,10 +1,14 @@
 module btb_file(
     input  logic         clk,
     input  logic [2:0]   read_index,
-    output logic [127:0] read_set,
     input  logic [2:0]   write_index,
     input  logic [127:0] write_set,
-    input  logic         write_enable
+    input  logic         write_enable,
+    input  logic [2:0]   update_index,
+    output logic [127:0] read_set,
+    output logic [127:0] update_set
+
+    
 );
     logic [127:0] btb_mem [7:0]; // 8 sets, 128 bits each
 
@@ -16,5 +20,8 @@ module btb_file(
         if(write_enable)
             btb_mem[write_index] <= write_set;
     end
-    
+
+    // Update (combinational)
+    assign update_set = btb_mem[update_index];
+
 endmodule
