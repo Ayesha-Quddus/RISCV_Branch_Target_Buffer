5bbc29d2314b1548a63e271d844eaeeeec8ddbed
Ayesha Quddus <ayeshaquddus111@gmail.com>
2025-10-11 12:57:37 +0100

    Added top-level BTB module

---
+++ src/btb.sv
@@ -0,0 +1,34 @@
+module btb(
+    input  logic        clk,
+    input  logic [31:0] PC,
+    input  logic        update,
+    input  logic [31:0] updatePC,
+    input  logic [31:0] updateTarget,
+    input  logic        mispredicted,
+    output logic        valid,
+    output logic [31:0] target,
+    output logic        predictedTaken
+);
+
+    // ----------- IF Stage -------------
+    // PC (32 bits) = Tag (27 bits) + Index (3 bits) + Byte offset (2 bits)
+    logic [2:0] index = PC[4:2];
+    logic [26:0] tag  = PC[31:5];
+
+    // BTB memory
+    logic [127:0] read_set;
+    logic [127:0] write_set;
+    logic [2:0] write_index;
+
+    btb_file u_btb_file(
+        .clk(clk),
+        .read_index(index),
+        .read_set(read_set),
+        .write_index(write_index),
+        .write_set(write_set),
+        .write_enable(update)
+    );
+
+    
+
+endmodule
